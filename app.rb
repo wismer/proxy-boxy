4eddb8d293e0fa0fead7ce93b219a747b37f437c
Matt Long <matthewhl@gmail.com>
2014-09-22 22:48:20 -0400

    remove magic stuff; might be causing 504

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require 'sinatra/base'
 require 'rss'
 require 'json'
-require 'sequel'
 
 class Blog < Sinatra::Base; end
 
@@ -20,10 +19,6 @@ class Blog
     erb :not_found
   end
 
-  get "/projects/magic" do
-    erb :magic
-  end
-
   get "/about" do
     erb :intro
   end
@@ -32,14 +27,14 @@ class Blog
     rss = RSS::Maker.make("2.0") do |maker|
       maker.channel.language = "en"
       maker.channel.author = "Matt Long"
-      maker.channel.updated = Time.new(2014, 9, 8)
+      maker.channel.updated = Time.new(2014, 9, 22)
       maker.channel.link = "http://lankstrosity.us/new.rss"
-      maker.channel.title = "Parsing"
-      maker.channel.description = "Parsing a HUGE file"
+      maker.channel.title = ""
+      maker.channel.description = ""
       maker.items.new_item do |item|
-        item.link = "http://lankstrosity.us/blog#parser"
-        item.title = "Parsing and its Malcontents"
-        item.updated = Time.new(2014, 9, 8).to_s
+        item.link = "http://lankstrosity.us/blog#threads"
+        item.title = "Revisiting Ruby's Thread Class"
+        item.updated = Time.new(2014, 9, 22).to_s
       end
     end
 
@@ -48,18 +43,5 @@ class Blog
 
   get "/:anything" do
     erb :not_found
-  end
-
-  post "/projects/magic/profile/create" do
-
-    id = params[:profileName]
-    redirect to("/projects/magic/profile?name=#{id}")
-  end
-
-  get "/projects/magic/profile" do
-    erb :profile
-  end
-
-  post "/magic/deck/:cards" do
   end
 end
