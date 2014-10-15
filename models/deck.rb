2bce9cfb16f5a376c8df97b19c2eb1c17e7e1b11
Matt Long <matthewhl@gmail.com>
2014-10-15 14:56:30 -0400

    add button html and reformat default json function

--- models/deck.rb
+++ models/deck.rb
@@ -1,3 +1,15 @@
 class Deck < ActiveRecord::Base
   has_many :cards
+
+  def button
+    "<button value='#{self.id}' exp='#{self.expansion}'>Show Card Breakdown</button>"
+  end
+
+  def cards_to_json
+    cards = self.cards
+    hash = {}
+
+    cards.each { |card| hash[card.multiverseid] = card.card_count }
+    JSON.generate(hash)
+  end
 end
