ce3e1ec322e125af15bd7a30218be480f4801496
Matt Long <matthewhl@gmail.com>
2014-10-15 14:57:35 -0400

    does swapping out order in require.js prioritize ext library? probably not

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,12 @@ requirejs.config({
   baseUrl: "js/lib",
   paths: {
     app: "../app",
-    jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min",
-    d3: "http://d3js.org/d3.v3.min",
     underscore: "../lib/underscore-min",
     backbone: "../lib/backbone-min",
-    draft: "../draft"
+    jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min",
+    d3: "http://d3js.org/d3.v3.min",
+    draft: "../draft",
+    decks: "../decks"
   }
 })
 
